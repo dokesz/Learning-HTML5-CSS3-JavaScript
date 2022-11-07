4de8598677fed8438df5af43804a73647d8ebe83
dokesz <dokesz.uzleti@gmail.com>
2022-11-07 12:38:00 +0100

    example for object

---
+++ Introduction to JS/object/script.js
@@ -0,0 +1,21 @@
+//object
+var user = {
+    name: "John",
+    age: 34,
+    hobby: "Soccer",
+    isMarried: false,
+    //we can use an array inside an object
+    spells: ["abrakadabra", "boom", "flash"]
+};
+
+//array --- we can use objects inside arrays
+var list = [
+    {
+        username: "andy",
+        password: "secret"
+    },
+    {
+        username: "john",
+        password: "1234"
+    }
+];
