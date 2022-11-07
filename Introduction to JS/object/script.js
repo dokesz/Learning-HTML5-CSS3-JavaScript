3686c4485731b31aaa0c1e81090a8460e5357552
dokesz <dokesz.uzleti@gmail.com>
2022-11-07 12:41:13 +0100

    function inside object

--- Introduction to JS/object/script.js
+++ Introduction to JS/object/script.js
@@ -5,7 +5,11 @@ var user = {
     hobby: "Soccer",
     isMarried: false,
     //we can use an array inside an object
-    spells: ["abrakadabra", "boom", "flash"]
+    spells: ["abrakadabra", "boom", "flash"],
+    //we can use functions here too
+    shout: function() {
+        console.log("AHHHH!");
+    }
 };
 
 //array --- we can use objects inside arrays
