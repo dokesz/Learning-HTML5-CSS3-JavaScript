e741c61dadd490cee6bd9cb027e34668d2d1c0d2
dokesz <dokesz.uzleti@gmail.com>
2022-11-07 10:20:11 +0100

    function declaration vs function expression

--- Introduction to JS/excersice5/script.js
+++ Introduction to JS/excersice5/script.js
@@ -1,5 +1,6 @@
 var age = prompt("What is your age?");
 
+//function declaration
 function checkDriverAge() {
     if(age < 18) {
         alert("Sorry you are too yound to drive this car. Powering off");
@@ -11,5 +12,18 @@ function checkDriverAge() {
 }
 
 checkDriverAge(age);
+
+//function expression
+let checkDriverAge2 = function() {
+    if(age < 18) {
+        console.log("Sorry you are too yound to drive this car. Powering off");
+    }
+    else if (age > 18) {
+        console.log("Powering On. Enjoy the ride!");
+    }
+    else console.log("Congratulations on your first year of driving. Enjoy the ride!");
+}
+
+checkDriverAge2();
 
 console.log(age);
