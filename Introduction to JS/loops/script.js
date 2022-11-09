13bacd33049064ff215eb7a203935f5c7486a44b
dokesz <dokesz.uzleti@gmail.com>
2022-11-09 14:21:14 +0100

    simple for loop

---
+++ Introduction to JS/loops/script.js
@@ -0,0 +1,12 @@
+var todos = [
+    "cleen room",
+    "brush teeth", 
+    "exercise",
+    "study javascript",
+    "eat healthy"
+];
+
+for (var i=0; i < todos.length; i++){
+    var word = todos[i] + "!";
+    console.log(word);
+}
