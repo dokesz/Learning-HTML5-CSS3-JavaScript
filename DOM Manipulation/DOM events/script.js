6ab3840a136e33bd3ebceabfa52d1c6aba0c7bd9
dokesz <lacivagyok3@gmail.com>
2022-11-18 09:58:15 +0100

    add elements with delete button

--- DOM Manipulation/DOM events/script.js
+++ DOM Manipulation/DOM events/script.js
@@ -8,19 +8,39 @@ button.addEventListener("mouseenter", function() {
     console.log("mouse enter!!");
 })*/
 
-var button = document.getElementById("enter");
+var enterButton = document.getElementById("enter");
+var deleteButton = document.getElementById("deleteable");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var toggle = document.getElementById("clickable");
 
 function inputValue() {
     return input.value;
 }
 
 function addElementToList() {
+    //create li element
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
+    //format input value
     input.value = "";
+    //create button for new elements
+    button = document.createElement("button");
+    var button_text = document.createTextNode("Delete"); 
+    button.appendChild(button_text);
+    ul.appendChild(button);
+    //delete new items
+    button.addEventListener("click", function(){
+        var item = document.getElementById("delete");
+        console.log(li);
+        item.parentNode.removeChild(li);
+        item.parentNode.removeChild(button);
+    })
+    //if list item is ready or done, cross line
+    li.addEventListener("click", function () {
+        li.style.textDecoration = "line-through";
+    });
 }
 
 function addElementAfterClick() {
@@ -35,6 +55,15 @@ function addElementAfterEnter() {
     }
 }
 
-button.addEventListener("click", addElementAfterClick)
+function removeElementFromList() {
+    /*var li = document.getElementById("delete");
+    console.log(li);
+    //var ul = document.getElementById("deleteable");
+    li.parentNode.removeChild(li);*/
+}
 
-input.addEventListener("keypress", addElementAfterEnter)
+enterButton.addEventListener("click", addElementAfterClick);
+
+input.addEventListener("keypress", addElementAfterEnter);
+
+deleteButton.addEventListener("click", removeElementFromList);
