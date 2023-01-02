c048066a9f04a8bdf0d1fac5b02139aba2aacaff
dokesz <lacivagyok3@gmail.com>
2023-01-02 19:20:48 +0100

    ternary function

--- advanced_JS/advanced_exercises/exercise2.js
+++ advanced_JS/advanced_exercises/exercise2.js
@@ -1,34 +1,40 @@
 //#1 change this function into a ternary and assign it to variable called experiencePoints
-function experiencePoints() {
-    if (winBattle()) {
-        return 10;
-    } else {
-        return 1;
-    }
+/*function experiencePoints() {
+  if (winBattle()) {
+    return 10;
+  } else {
+    return 1;
+  }
+}*/
+
+let experiencePoints = winBattle(true) ? 10 : 1;
+
+function winBattle(bool){
+    return bool;
 }
 
 //Using this function, answer the questions below:
 function moveCommand(direction) {
-    var whatHappens;
-    switch (direction) {
-        case "forward":
-            break;
-            whatHappens = "you encounter a monster";
-        case "back":
-            whatHappens = "you arrived home";
-            break;
-            break;
-        case "right":
-            return whatHappens = "you found a river";
-            break;
-        case "left":
-            break;
-            whatHappens = "you run into a troll";
-            break;
-        default:
-            whatHappens = "please enter a valid direction";
-    }
-    return whatHappens;
+  var whatHappens;
+  switch (direction) {
+    case "forward":
+      break;
+      whatHappens = "you encounter a monster";
+    case "back":
+      whatHappens = "you arrived home";
+      break;
+      break;
+    case "right":
+      return (whatHappens = "you found a river");
+      break;
+    case "left":
+      break;
+      whatHappens = "you run into a troll";
+      break;
+    default:
+      whatHappens = "please enter a valid direction";
+  }
+  return whatHappens;
 }
 
 //#2 return value when moveCommand("forward");
