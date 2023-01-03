0666b5dc55e2b9fded369ef958a811e0e2b80777
dokesz <lacivagyok3@gmail.com>
2023-01-03 16:21:21 +0100

    Object properties

--- advanced_JS/ES5_and_ES6.js
+++ advanced_JS/ES5_and_ES6.js
@@ -1,23 +1,23 @@
 //let + const
 
-const player = 'bobby';
+const player = "bobby";
 let experience = 100;
 let wizardLevel = false;
 
-if (experience > 90){
-    let wizardLevel = true;
-    console.log('inside', wizardLevel);
+if (experience > 90) {
+  let wizardLevel = true;
+  console.log("inside", wizardLevel);
 }
 
-console.log('outside', wizardLevel);
+console.log("outside", wizardLevel);
 
 //Destructuring
 
 const obj = {
-    player: 'Bobby',
-    experience: 100,
-    wizardLevel: false
-}
+  player: "Bobby",
+  experience: 100,
+  wizardLevel: false,
+};
 
 /*const player1 = obj.player;
 const experience1 = obj.experience;*/
@@ -25,3 +25,22 @@ const experience1 = obj.experience;*/
 
 const { player1, experience1 } = obj;
 let { wizardLevel1 } = obj;
+
+//Object properties
+
+const name = "john snow";
+
+const obj1 = {
+  [name]: "hello",
+  ["ray" + "smith"]: "hihi",
+};
+
+const a = "simon";
+const b = true;
+const c = {};
+
+const obj2 = {
+    a,
+    b,
+    c
+}
