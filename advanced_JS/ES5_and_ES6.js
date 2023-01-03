f2c92388018141032d83f5310d3453d35520c461
dokesz <lacivagyok3@gmail.com>
2023-01-03 16:15:09 +0100

    Destructuring

--- advanced_JS/ES5_and_ES6.js
+++ advanced_JS/ES5_and_ES6.js
@@ -10,3 +10,18 @@ if (experience > 90){
 }
 
 console.log('outside', wizardLevel);
+
+//Destructuring
+
+const obj = {
+    player: 'Bobby',
+    experience: 100,
+    wizardLevel: false
+}
+
+/*const player1 = obj.player;
+const experience1 = obj.experience;*/
+//let wizardLevel1 = obj.wizardLevel;
+
+const { player1, experience1 } = obj;
+let { wizardLevel1 } = obj;
