7dd4d54c14868932baf5b61740181498bfb3aecf
dokesz <lacivagyok3@gmail.com>
2023-01-04 19:40:39 +0100

    sumBy5

--- advanced_JS/advanced_functions.js
+++ advanced_JS/advanced_functions.js
@@ -1,5 +1,6 @@
 const multiply = (a,b) => a+b;
 
-const carriedMultiply = (a) => (b) => a+b;
+const carriedSum = (a) => (b) => a+b;
 //to use carreidMultiply, use chrome developer option and write carreidMultiply(number)(number)
- 
+
+const sumBy5 = carriedSum(5);
