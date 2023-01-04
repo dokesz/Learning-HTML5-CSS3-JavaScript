d3c662b57aa3370f8333f44ed08dd7b6b68ea0b4
dokesz <lacivagyok3@gmail.com>
2023-01-04 19:46:54 +0100

    compose

--- advanced_JS/advanced_functions.js
+++ advanced_JS/advanced_functions.js
@@ -4,3 +4,10 @@ const carriedSum = (a) => (b) => a+b;
 //to use carreidMultiply, use chrome developer option and write carreidMultiply(number)(number)
 
 const sumBy5 = carriedSum(5);
+
+//compose
+const compose = (f,g) => (a) => f(g(a));
+
+const sum = (num) => num + 1;
+
+compose(sum,sum)(5);
