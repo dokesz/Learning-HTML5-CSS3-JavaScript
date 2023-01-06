a1565594863b5fe2e326300d96ba0e8ed030b122
dokesz <lacivagyok3@gmail.com>
2023-01-06 13:19:40 +0100

    JSON.parse(JSON.stringify())

--- advanced_JS/passByReference_passByValue.js
+++ advanced_JS/passByReference_passByValue.js
@@ -27,11 +27,19 @@ console.log(c);
 
 //object.assign()
 
-let obj = { a: "a", b: "b", c: "c" };
+let obj = {
+  a: "a",
+  b: "b",
+  c: /*"c" ,*/ {
+    deep: "try and copy me",
+  },
+};
 let clone = Object.assign({}, obj);
-let clone2 = {...obj};
+let clone2 = { ...obj };
+let superClone = JSON.parse(JSON.stringify(obj));
 
-obj.c = 5;
+obj.c.deep = 'hahaha';
 console.log(obj);
 console.log(clone);
 console.log(clone2);
+console.log(superClone);
