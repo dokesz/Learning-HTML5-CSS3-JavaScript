047cb6558aa09ce67deaee2a3c5a86140c3da442
dokesz <dokesz.uzleti@gmail.com>
2023-01-07 13:15:54 +0100

    Object.keys

--- advanced_JS/ES8.js
+++ advanced_JS/ES8.js
@@ -2,3 +2,15 @@
 
 'Turtle'.padStart(10);
 'Turtle'.padEnd(10);
+
+// object.values, object.entries, object.keys
+
+let obj = {
+    username0: 'Mami',
+    username1: 'Papi',
+    username2: 'Peti'
+}
+
+Object.keys(obj).forEach((key, index) => {
+    console.log(key, obj[key]);
+})
