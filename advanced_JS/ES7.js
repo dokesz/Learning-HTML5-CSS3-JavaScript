32645afe8347454407d56b3e7f6c38a22e7b7ab7
dokesz <dokesz.uzleti@gmail.com>
2023-01-07 12:36:03 +0100

    .includes()

---
+++ advanced_JS/ES7.js
@@ -0,0 +1,9 @@
+// .includes()
+const arr = ['dog', 'cat', 'bird'];
+
+arr.includes('dog'); // return true
+arr.includes('bat'); // return false
+
+let string = 'Helloooo';
+
+string.includes('o'); // reutrn true
