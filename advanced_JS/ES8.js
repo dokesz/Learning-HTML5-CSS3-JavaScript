829d133a58ba66f05cf2953707b799dc7712cdbe
dokesz <dokesz.uzleti@gmail.com>
2023-01-07 13:26:06 +0100

    change usernames

--- advanced_JS/ES8.js
+++ advanced_JS/ES8.js
@@ -1,24 +1,28 @@
 // padStart(), padEnd()
 
-'Turtle'.padStart(10);
-'Turtle'.padEnd(10);
+"Turtle".padStart(10);
+"Turtle".padEnd(10);
 
 // object.values, object.entries, object.keys
 
 let obj = {
-    username0: 'Mami',
-    username1: 'Papi',
-    username2: 'Peti'
-}
+  username0: "Mami",
+  username1: "Papi",
+  username2: "Peti",
+};
 
 Object.keys(obj).forEach((key, index) => {
-    console.log(key, obj[key]);
-})
+  console.log(key, obj[key]);
+});
 
-Object.values(obj).forEach(value => {
-    console.log(value, obj[value]);
-})
+Object.values(obj).forEach((value) => {
+  console.log(value, obj[value]);
+});
 
-Object.entries(obj).forEach(entrie => {
-    console.log(entrie);
-})
+Object.entries(obj).forEach((entrie) => {
+  console.log(entrie);
+});
+
+Object.entries(obj).map((value) => {
+  return value[1] + value[0].replace("username", "");
+});
