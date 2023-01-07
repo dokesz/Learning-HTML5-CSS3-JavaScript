3ce21ec16ebd3191e42a7fa87485684e1a3d3183
dokesz <dokesz.uzleti@gmail.com>
2023-01-07 12:37:58 +0100

    exponential operator

--- advanced_JS/ES7.js
+++ advanced_JS/ES7.js
@@ -7,3 +7,10 @@ arr.includes('bat'); // return false
 let string = 'Helloooo';
 
 string.includes('o'); // reutrn true
+
+// exponential operator
+
+let square = (x) => x**2;
+let cube = (y) => y**3;
+
+console.log(cube(2), square(2));
