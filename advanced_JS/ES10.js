5c288b80c0f07f6cf861895728c9082f426b035c
dokesz <lacivagyok3@gmail.com>
2023-01-09 10:56:03 +0100

    fromEntries()

--- advanced_JS/ES10.js
+++ advanced_JS/ES10.js
@@ -9,8 +9,17 @@ const array3 = [1,2,[3,4,[5]]];
 // i can tell the method how many layers i want to fly in the array (default is 1, but its not working here)
 array3.flat(2);
 
+// .trimStart(), .trimEnd()
 const userEmail = "        eddytheeagle@gmail.com";
 const userEmail2 = 'jonnysmt@gmail.com     ';
 
 console.log(userEmail.trimStart());
 console.log(userEmail2.trimEnd()); 
+
+// fromEntries
+
+userProfiles = [['commanderTom', 23], ['derekZlander', 40], ['hansel', 18]];
+
+const obj = Object.fromEntries(userProfiles);
+Object.entries(obj);
+
